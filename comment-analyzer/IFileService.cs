95a6ee1dc9b66e972c95ba458d411621af1903fa
Dmitry <purpl3mint@yandex.ru>
2021-04-28 20:38:09 +0700

    Создан каркас программы

---
+++ comment-analyzer/IFileService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace comment_analyzer
+{
+    interface IFileService
+    {
+        string ReadFile(string filePath);
+        void SaveFile(string filePath, string data);
+    }
+}
